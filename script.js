f1ceb59a8ce72b28e09530c69276b3c9cb12ad8d
thomasmfx <thomasmoisesf@gmail.com>
2024-06-14 21:48:13 -0300

    feat: Add function to check game result

--- script.js
+++ script.js
@@ -26,9 +26,10 @@ const game = (function() {
 
     function playRound(player, row, index) {
         if(isIndexAvailable(index) === true) {
-            if(gameBoard.board[`${row}`][index] < 2) {
+            if(gameBoard.board[`${row}`][index] <= 2) {
                 gameBoard.board[`${row}`][index] = player.marker;
                 showBoard();
+                checkResult(player.name)
             } else {
                 console.log('Posição indisponível.')
             }
@@ -58,5 +59,52 @@ function createPlayer(name, marker) {
         getScore() {
             return score;
         }
+    };
+};
+
+function checkResult(player) {
+    const row1 = gameBoard.board['row1'],
+        row2 = gameBoard.board['row2'],
+        row3 = gameBoard.board['row3'];
+
+    // Rows
+    if((row1[0] === row1[1] && 
+        row1[1] === row1[2]) ||
+        (row2[0] === row2[1] && 
+        row2[1] === row2[2]) ||
+        (row3[0] === row3[1] && 
+        row3[1] === row3[2])) {
+        console.log(`${player} won!`)
+        return player.updateScore()
+    } 
+    
+    // Diagonal
+    if((row1[0] === row2[1] &&
+        row2[1] === row3[2]) ||
+        (row1[2] === row2[1] &&
+        row2[1] === row3[0])) {
+        console.log(`${player} won!`)
+        return player.updateScore()
+    } 
+    
+    //Columns (1-2-3)
+    if(row1[0] !== 0 && row2[0] !== 0 && row3[0] !== 0) {
+        if(row1[0] === row2[0] &&
+            row2[0] === row3[0]) {
+            console.log(`${player} won!`)
+            return player.updateScore()
+        };
+    } else if((row1[1] !== 1 && row2[1] !== 1 && row3[1] !== 1)) {
+        if((row1[1] === row2[1] &&
+            row2[1] === row3[1])) {
+            console.log(`${player} won!`)
+            return player.updateScore()
+        };
+    } else if((row1[2] !== 2 && row2[1] !== 2 && row3[2] !== 2)) {
+        if(row1[2] === row2[2] &&
+            row2[2] === row3[2]) {
+            console.log(`${player} won!`)
+            return player.updateScore()
+        };
     };
 };
