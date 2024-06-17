b6bc3a940a5572ecf33a47143f5c4154bd420a7b
thomasmfx <thomasmoisesf@gmail.com>
2024-06-17 17:09:43 -0300

    refactor: Change IIFEs name and move 'createPlayer' up
    IIFEs: 'game' to 'gameActions', 'gameBoard' to 'game'

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const gameBoard = (function() {
+const game = (function() {
     let board = {
         row1: [0, 1, 2],
         row2: [0, 1, 2],
         row3: [0, 1, 2]
     };
 
-    const restartBtn = getElement('#restart');
+    const restartBtn = document.querySelector('#restart');
     restartBtn.addEventListener("click", () => {
-        game.reset()
+        gameActions.reset()
     })
 
     return { board };
 })();
 
-const game = (function() {
+const gameActions = (function() {
     let lastTurn = null;
     const placeholders = document.querySelectorAll('.placeholder');
 
@@ -22,7 +22,7 @@ const game = (function() {
     };
     
     function showBoard() {
-        console.table(gameBoard.board);
+        console.table(game.board);
     };
 
     function isIndexAvailable(i) {
@@ -34,7 +34,7 @@ const game = (function() {
     };
 
     function reset() {
-        gameBoard.board = {
+        game.board = {
             row1: [0, 1, 2],
             row2: [0, 1, 2],
             row3: [0, 1, 2]
@@ -52,7 +52,7 @@ const game = (function() {
             if(players.p1 !== undefined && players.p2 !== undefined) {
                 let img = placeholder.firstElementChild.src;
                 if(!img.includes('svg')) {
-                    game.playRound(players.p1, row, index, players.p2);
+                    gameActions.playRound(players.p1, row, index, players.p2);
                     placeholder.firstElementChild.src = lastTurn.domMarker;
                     checkResult(lastTurn);
                 };
@@ -65,13 +65,13 @@ const game = (function() {
 
     function playRound(player1, row, index, player2) {
         if(isIndexAvailable(index) === true &&
-        gameBoard.board[`${row}`][index] <= 2) {
+        game.board[`${row}`][index] <= 2) {
             if(isPlayerTurn(player1) || lastTurn === null) {
-                gameBoard.board[`${row}`][index] = player1.marker;
+                game.board[`${row}`][index] = player1.marker;
                 showBoard();
                 lastTurn = player1;
             } else {
-                gameBoard.board[`${row}`][index] = player2.marker;
+                game.board[`${row}`][index] = player2.marker;
                 showBoard();
                 lastTurn = player2;
             };
@@ -83,59 +83,10 @@ const game = (function() {
     return { 
         getScore, 
         showBoard,
-        isIndexAvailable,
-        isPlayerTurn, 
         reset,
         playRound
     };
 })();
-
-function checkResult(player) {
-    const row1 = gameBoard.board['row1'],
-        row2 = gameBoard.board['row2'],
-        row3 = gameBoard.board['row3'];
-
-    // Rows
-    if((row1[0] === row1[1] && 
-        row1[1] === row1[2]) ||
-        (row2[0] === row2[1] && 
-        row2[1] === row2[2]) ||
-        (row3[0] === row3[1] && 
-        row3[1] === row3[2])) {
-        console.log(`${player.name} won!`);
-        return player.updateScore(player), game.reset(), game.getScore();
-    } 
-    
-    // Diagonal
-    if((row1[0] === row2[1] &&
-        row2[1] === row3[2]) ||
-        (row1[2] === row2[1] &&
-        row2[1] === row3[0])) {
-        console.log(`${player.name} won!`);
-        return player.updateScore(player), game.reset(), game.getScore();
-    } 
-     
-    //Columns
-    if(row1[0] !== 0 && row2[0] !== 0 && row3[0] !== 0) {
-        if(row1[0] === row2[0] &&
-            row2[0] === row3[0]) {
-            console.log(`${player.name} won!`);
-            return player.updateScore(player), game.reset(), game.getScore();
-        };
-    } else if((row1[1] !== 1 && row2[1] !== 1 && row3[1] !== 1)) {
-        if((row1[1] === row2[1] &&
-            row2[1] === row3[1])) {
-            console.log(`${player.name} won!`);
-            return player.updateScore(player), game.reset(), game.getScore();
-        };
-    } else if((row1[2] !== 2 && row2[2] !== 2 && row3[2] !== 2)) {
-        if(row1[2] === row2[2] &&
-            row2[2] === row3[2]) {
-            console.log(`${player.name} won!`);
-            return player.updateScore(player), game.reset(), game.getScore();
-        };
-    };
-};
 
 const players = (function() {
     let p1;
@@ -197,6 +148,53 @@ const players = (function() {
     }
 })();
 
+function checkResult(player) {
+    const row1 = game.board['row1'],
+        row2 = game.board['row2'],
+        row3 = game.board['row3'];
+
+    // Rows
+    if((row1[0] === row1[1] && 
+        row1[1] === row1[2]) ||
+        (row2[0] === row2[1] && 
+        row2[1] === row2[2]) ||
+        (row3[0] === row3[1] && 
+        row3[1] === row3[2])) {
+        console.log(`${player.name} won!`);
+        return player.updateScore(player), gameActions.reset(), gameActions.getScore();
+    } 
+    
+    // Diagonal
+    if((row1[0] === row2[1] &&
+        row2[1] === row3[2]) ||
+        (row1[2] === row2[1] &&
+        row2[1] === row3[0])) {
+        console.log(`${player.name} won!`);
+        return player.updateScore(player), gameActions.reset(), gameActions.getScore();
+    } 
+     
+    //Columns
+    if(row1[0] !== 0 && row2[0] !== 0 && row3[0] !== 0) {
+        if(row1[0] === row2[0] &&
+            row2[0] === row3[0]) {
+            console.log(`${player.name} won!`);
+            return player.updateScore(player), gameActions.reset(), gameActions.getScore();
+        };
+    } else if((row1[1] !== 1 && row2[1] !== 1 && row3[1] !== 1)) {
+        if((row1[1] === row2[1] &&
+            row2[1] === row3[1])) {
+            console.log(`${player.name} won!`);
+            return player.updateScore(player), gameActions.reset(), gameActions.getScore();
+        };
+    } else if((row1[2] !== 2 && row2[2] !== 2 && row3[2] !== 2)) {
+        if(row1[2] === row2[2] &&
+            row2[2] === row3[2]) {
+            console.log(`${player.name} won!`);
+            return player.updateScore(player), gameActions.reset(), gameActions.getScore();
+        };
+    };
+};
+
 function createElementWithId(el, id, text) {
     let element = document.createElement(`${el}`);
     element.id = id;
@@ -209,8 +207,4 @@ function createElementWithClass(el, cl, text) {
     element.classList.add(cl);
     element.textContent = text;
     return element;
-};
-
-function getElement(el) {
-    return document.querySelector(`${el}`);
 };
